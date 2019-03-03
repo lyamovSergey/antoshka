adc9e72d241aedd3fcd5a0632a52130915ba8ea3
lyamovSergey <lyamofff@gmail.com>
2019-03-03 17:43:19 +0200

    add modal, add responsive

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 window.onload = function() {
+    var input = document.getElementsByClassName('inp');
     var phoneNumber = document.getElementById('phone');
     var mail = document.getElementById('mail');
     var form = document.getElementById('offer__form');
     var load = document.getElementById('loader');
     var body = document.body;
-
+    var modal = document.getElementById('modal');
+    var modalText = document.getElementById('modal__text');
+    var modalBtn = document.getElementById('modal__button');
+    //valid email
     function validMail(email) {
         var regularMail = /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i;
         var email = regularMail.test(email.value);
@@ -15,7 +19,7 @@ window.onload = function() {
         mailError.innerHTML = 'Введите правильный адрес';
         return false;
     };
-
+    //valid phone number
     function validPhone(num) {
         var regularPhone = /^[\d]{10}$/;
         var phone = regularPhone.test(num.value);
@@ -26,7 +30,7 @@ window.onload = function() {
         phoneError.innerHTML = 'Введите правильный номер';
         return false;
     };
-
+    //loader & block html true/false
     function loader(loadeProcess) {
         if (loadeProcess) {
             load.style = 'display: block';
@@ -38,38 +42,52 @@ window.onload = function() {
         }
 
     }
-
+    //show modal true/false    
+    function showMessage(param) {
+        if (param) {
+            modalText.innerHTML = 'Регистрация прошла успешно!';
+            modal.style = 'display: flex';
+            for (var i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        } else {
+            modalText.innerHTML = 'Ошибка регистрации!';
+            modal.style = 'display: flex';
+        }
+        modalBtn.onclick = function() {
+            modal.style = 'display: none';
+            loader(false);
+        }
+    }
+    //ajax
     function sendData(data) {
         var xhr = new XMLHttpRequest();
-        // var param = JSON.stringify(data);
-        // xhr.responseType = 'json';
         xhr.open('POST', 'http://sw.ants.co.ua/demo/', true);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        // xhr.setRequestHeader('Access-Control-Allow-Origin', '');
         xhr.onload = function() {
-            console.log(xhr);
-            // do something with jsonResponse
+            if (xhr.status != 200) {
+                alert(xhr.status + ': ' + xhr.statusText);
+            } else {
+                console.log(xhr.responseText);
+                //if response == true -> run showMessage(true);...
+            };
         };
-        console.log("data", data);
         xhr.send(data);
     };
-
+    //form send
     form.onsubmit = function() {
         var email = validMail(mail);
         var phone = validPhone(phoneNumber);
         if (phone && email) {
             loader(true);
-            var timer = setTimeout(function(){
-                loader(false);
+            var timer = setTimeout(function() {
+                showMessage(false);
             }, 3000);
-            var input = document.getElementsByClassName('inp');
             var data = '';
             for (var i = 0; i < input.length; i++) {
                 data += input[i].name + '=' + encodeURIComponent(input[i].value) + '&';
             }
-            // var param = JSON.stringify(data);
-            console.log(typeof param);
-            var param = encodeURIComponent("couponcode=0087-0220302018&name=111&mail=asd@gmail.com&phoneNumber=1111111111&lastName=asd");
-
             sendData(data);
         }
         return false;
